7815483bd6c7c0ec05a20922af5459391ce3dc17
YtoHasegawa <yto.hasegawa@gmail.com>
2025-10-18 18:37:21 +0900

    test06

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 
 // 背景用のテクスチャ
 const textureLoader = new THREE.TextureLoader();
-const bgTexture = textureLoader.load("bg/bg.JPG");
+const bgTexture = textureLoader.load("./bg/bg.JPG");
 scene.background = bgTexture;
 
 // サイズ
