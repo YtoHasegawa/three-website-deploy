365705fad3f25721846ce46ea6ca640b34c32271
YtoHasegawa <yto.hasegawa@gmail.com>
2025-10-18 18:43:56 +0900

    test08

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from "three"
-import bg from "./public/bg.jpg"
+import bg from "./public/bg.JPG"
 
 // canvas
 const canvas = document.querySelector('#webgl');
